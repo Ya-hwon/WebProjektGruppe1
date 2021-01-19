b740cb610c2bd1e5577a80a2145aee5321b89994
nicolasstyrnol <77490395+nicolasstyrnol@users.noreply.github.com>
2021-01-19 17:04:10 +0100

    added basic structure
    added basic files
    added basic folder structure
    added booststrap
    added JQuery
    added Animate.css Libary

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include('./header.php');
+?>
+
+<main>
+    <h2>Ich bin eine Überschrift</h2>
+    <p>Ich bin ein Absatz im Hauptteil!</p>
+    <button type="button" class="btn btn-primary">Primary</button>
+</main>
+
+
+<?php
+include('./footer.php');
+?>
---
+++ template.php
@@ -0,0 +1,14 @@
+<?php
+include('./header.php');
+?>
+
+<main>
+    <h2>Template</h2>
+    <p>Ich bin ein Absatz im Hauptteil!</p>
+    <button type="button" class="btn btn-primary">Primary</button>
+</main>
+
+
+<?php
+include('./footer.php');
+?>
