cb98967cb29f18abee24fcaedc866fda07b9d80f
Sarah Hägele <sarah.haegele01@gmail.com>
2021-01-20 16:10:13 +0100

    First version of page "Unser Verein"
    - Created the page "Unser Verein"
    - Added and styled four cards with placeholder images and added info from BTV-Website

---
+++ unserverein.php
@@ -0,0 +1,102 @@
+<?php
+include('./header.php');
+?>
+
+<link rel="stylesheet" href="src/css/unserverein.css">
+
+<main>
+    <h2>Mitarbeiter</h2>
+    
+<div class="card mb-3" style="max-width: 540px">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img
+        src="src/img/placeholder-person.jpg"
+        alt="Foto von Stefan Kauffmann"
+        class="img-fluid"/>
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h4 class="card-title">Stefan Kauffmann</h4>
+        <p class="card-subtitle">Geschäftsführer</p>
+        <p class="card-text">
+          Tel.: 07321 943060<br/>
+          Fax: 07321 9469577</p>
+          <button type="button" class="btn btn-primary">Email</button>
+      </div>
+    </div>
+  </div>
+</div>
+    
+<div class="card mb-3" style="max-width: 540px">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img
+        src="src/img/placeholder-person.jpg"
+        alt="Foto von Heide Bolsinger"
+        class="img-fluid"/>
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h4 class="card-title">Heide Bolsinger</h4>
+        <p class="card-subtitle">Vereinsbetreuerin</p>
+        <p class="card-text">
+          Tel.: 07321 943062<br/>
+          Fax: 07321 9469577</p>
+          <button type="button" class="btn btn-primary">Email</button>
+      </div>
+    </div>
+  </div>
+</div>
+    
+<div class="card mb-3" style="max-width: 540px">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img
+        src="src/img/placeholder-person.jpg"
+        alt="Foto von Michaela Schmidt"
+        class="img-fluid"/>
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h4 class="card-title">Michaela Schmidt</h4>
+        <p class="card-subtitle">Vereinsbetreuerin</p>
+        <p class="card-text">
+          Tel.: 07321 9469578<br/>
+          Fax: 07321 9469577</p>
+          <button type="button" class="btn btn-primary">Email</button>
+      </div>
+    </div>
+  </div>
+</div>
+    
+<div class="card mb-3" style="max-width: 540px">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img
+        src="src/img/placeholder-person.jpg"
+        alt="Foto von Christine Bäuerle"
+        class="img-fluid"/>
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h4 class="card-title">Christine Bäuerle</h4>
+        <p class="card-subtitle">Verwaltung</p>
+        <p class="card-text">
+          Tel.: 07321 943061<br/>
+          Fax: 07321 9469577</p>
+          <button type="button" class="btn btn-primary">Email</button>
+      </div>
+    </div>
+  </div>
+</div>
+    
+<h2><br/>Vorstand</h2>
+<img id="photo-vorstand" src="src/img/vorstand.jpg" alt="Gruppenfoto des Vorstands">
+    
+</main>
+
+
+<?php
+include('./footer.php');
+?>
