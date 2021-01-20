32edddda1ccea01801f1356ef88eb8811df18004
JohannesAuberger <77609143+JohannesAuberger@users.noreply.github.com>
2021-01-20 13:18:56 +0100

    Update betreuung.php
    imported content to betreuung.php
    (copied the text from the original website)

--- betreuung.php
+++ betreuung.php
@@ -20,19 +20,20 @@ include('./header.php');
 		<li><a href="#Einwilligungsvorbehalt">9. Einwilligungsvorbehalt</a></li>
 		<li><a href="#Ansprüche">10. Ansprüche des Betreuers</a></li>
 		<li><a href="#Ende">11. Ende der Betreuung</a></li>
+		<li><a href="#Betreuungsgerichte">12. Betreuungsgerichte</a></li>
 	</ul>	
-	<h3>1. Grundzüge des Betreuungsgesetzes</h3>
+	<h3 id="Grundzüge">1. Grundzüge des Betreuungsgesetzes</h3>
 	<p>Das Betreuungsgesetz hat zum 01.01.1992 das frühere Recht der Vormundschaften und Gebrechlichkeitspflegschaften für Erwachsene abgelöst.<br/><br/>
 	Die Rechte der betroffenen Personen wurden insbesondere im Bereich der Personensorge und im Gerichtsverfahren gestärkt. 
 	Die Betreuung wird nur noch in den Bereichen angeordnet, die der Betroffene selbst nicht mehr regeln kann (Erforderlichkeitsprinzip).
 	Die Betreuung hat grundsätzlich keinen Einfluss auf die Geschäftsfähigkeit der betreuten Person. Das Wahlrecht sowie die Möglichkeiten zur Eheschließung und zur Testamentserrichtung bleiben erhalten.<br/><br/>
 	Wünsche des Betreuten sind vom Betreuer grundsätzlich zu beachten, soweit diese ihm zumutbar sind und dem Wohl der betroffenen Person nicht zuwiderlaufen.<br/><br/>
 	Das Betreuungsgesetz wurde bislang viermal reformiert.</p>
-	<h3>2. Gesetzliche Grundlagen</h3>
+	<h3 id="Gesetzliche Grundlagen">2. Gesetzliche Grundlagen</h3>
 	<p>Das materielle Betreuungsrecht ist im Bürgerlichen Gesetzbuch (BGB) geregelt. Insbesondere im Bereich der Vermögenssorge finden sich viele Querverweise auf das Vormundschaftsrecht für Minderjährige.<br/><br/>
 	Das Betreuungsverfahren ist seit 01.09.2009 im Gesetz über das Verfahren in Familiensachen und in den Angelegenheiten der freiwilligen Gerichtsbarkeit geregelt (FamFG).<br/><br/>
 	Die Aufgaben der örtlichen Betreuungsbehörde finden sich im Betreuungsbehördengesetz, die Bestimmungen zur Bezahlung von Berufsbetreuern im Gesetz über die Vergütung von Berufsbetreuern und Berufsvormündern.</p>
-	<h3>3. Voraussetzungen einer Betreuung</h3>
+	<h3 id="Voraussetzungen">3. Voraussetzungen einer Betreuung</h3>
 	<p>Für die Anordnung einer Betreuung muss eine psychische Krankheit, eine körperliche, geistige oder seelische Behinderung vorliegen. 
 	Zudem muss diese dazu führen, dass der Betroffene seine Angelegenheiten ganz oder teilweise nicht mehr besorgen kann. 
 	Eine Betreuung soll nicht angeordnet werden, sofern sonstige Hilfen durch Angehörige, soziale Dienste oder Bevollmächtigte ausreichen.<br/><br/>
@@ -40,6 +41,91 @@ include('./header.php');
 	Gegen den freien Willen des Betroffenen darf keine Betreuung angeordnet werden.<br/><br/>
 	Bei einer rein körperlichen Behinderung ist die Zustimmung der betroffenen Person erforderlich.<br/><br/>
 	Die Betreuungsmaßnahme darf für längstens 7 Jahre angeordnet werden.</p>
+	<h3 id="Betreuungsverfahren">4. Das Betreuungsverfahren</h3>
+	<p>Zuständig ist das Betreuungsgericht, welches entweder auf Antrag des Betroffenen oder auf Anregung Dritter die Notwendigkeit einer Betreuungsmaßnahme überprüft. 
+	Im württembergischen Landesteil sind bis 31.12.2017 die Bezirksnotariate als Betreuungsgerichte für die Durchführung des Betreuungsverfahrens zuständig, 
+	im sonstigen Bundesgebiet sind dies die Amtsgerichte (Richter und Rechtspfleger).<br/><br/>
+	Das gerichtliche Verfahren sieht die Einholung eines nervenärztlichen Gutachtens vor (ersatzweise kann das Pflegegutachten des Medizinischen Dienstes herangezogen werden), 
+	die persönliche Anhörung des Betroffenen durch das Betreuungsgericht und in besonderen Fällen die Einbeziehung eines sog. Verfahrenspflegers (oftmals Rechtsanwälte).<br/><br/>
+	Seit 01.07.2014 muss die örtliche Betreuungsbehörde obligatorisch einen Sozialbericht (Sachverhaltsermittlung) erstellen, in dem zur Gesamtsituation, 
+	zum Hilfebedarf und zu evtl. anderen vorrangigen Hilfen Stellung genommen und ein geeigneter Betreuer vorgeschlagen wird.<br/><br/>
+	Die betreute Person kann gegen sämtliche Entscheidungen des Betreuungsgerichts Rechtsmittel einlegen.<br/><br/>
+	Die Betreuung kann nur für eine volljährige Person angeordnet werden. Ausnahme: Vorsorgliche Betreuerbestellung für Minderjährige (§ 1908 a BGB): 
+	Wenn anzunehmen ist, dass bei Eintritt der Volljährigkeit eine Betreuung notwendig ist, kann diese bereits mit Vollendung des 17. Lebensjahres geprüft und angeordnet werden. 
+	Die Maßnahme wird dann mit Erreichen der Volljährigkeit sofort wirksam.<br/><br/>
+	In Eilfällen ist eine vorläufige Betreuungseinrichtung möglich (max. 1 Jahr)</p>
+	<h3 id ="Aufgabenkreise">5. Aufgabenkreise</h3>
+	<p>Die Betreuung wird nicht grundsätzlich für alle Angelegenheiten angeordnet, sondern nur für die konkret erforderlichen Aufgabenkreise.<br/><br/>
+	Die häufigsten Aufgabenkreise sind: Vermögenssorge, Gesundheitsfürsorge bzw. Einwilligung in Heilbehandlungen, Aufenthaltsbestimmung ggf. 
+	mit Einwilligung in freiheitsentziehende Maßnahmen, Wohnungs- und Heimangelegenheiten sowie Regelung des Postverkehrs im Rahmen der Aufgabenkreise.</p>
+	<h3 id="Auswahl">6. Auswahl des Betreuers</h3>
+	<p>Der Betreuer muss geeignet sein, die rechtlichen Angelegenheiten des Betroffenen zu besorgen und ihn dabei persönlich zu betreuen.<br/><br/>
+	Der Betreuer darf nicht in der Einrichtung tätig sein, in welcher der Betreute wohnt oder untergebracht ist.<br/><br/>
+	Die Betreuung soll vorrangig durch Ehrenamtliche (Angehörige oder sonstige Ehrenamtliche) übernommen werden. Eine besondere Qualifikation ist nicht erforderlich. Berufsbetreuer dürfen nur ausnahmsweise eingesetzt werden.
+	Eine gesetzliche Verpflichtung zur Betreuungsübernahme gibt es nicht, auch nicht für Familienangehörige.<br/><br/>
+	Bei der Betreuerauswahl ist es eine gewisse Reihenfolge zu beachten:<br/>
+	Die Person, die sich der Betroffene wünscht
+	<ul>
+	<li>Verwandte</li>
+	<li>Sonstige Ehrenamtliche</li>
+	<li>Berufsbetreuer (entweder freiberufliche Betreuer oder hauptamtliche Mitarbeitereines Betreuungsvereins)</li>
+	<li>Örtliche Betreuungsbehörde, falls sonst niemand zur Verfügung steht.</li>
+	</ul></p>
+	<h3 id="Pflichten">7. Pflichten des Betreuers</h3>
+	<p>Die Betreuung sohl dem Wohl des Betreuten dienen, dessen Wünsche und Interessen sind zu beachten, solange sie dessen Wohl nicht zuwiderlaufen und dem Betreuer zuzumuten sind. 
+	Der Betreute soll sein Leben weiterhin nach eigenen Vorstellungen gestalten können.<br/><br/>
+	Der Betreuer muss wichtige Angelegenheiten mit dem Betreuten besprechen (soweit möglich).<br/><br/>
+	Der Betreuer vertritt den Betreuten gerichtlich und außergerichtlich.<br/><br/>
+	Der Betreuer hat einen Rehabilitationsauftrag, d.h. er soll dazu beitragen, dass Krankheiten oder Behinderungen beseitigt oder gemildert werden.<br/><br/>
+	Obliegt dem Betreuer die Vermögenssorge, so muss er zu Beginn der Betreuung eine Vermögensübersicht erstellen und jährlich dem Betreuungsgericht Rechnung legen, d.h. Einnahmen und Ausgaben mit Belegen nachweisen. 
+	Hierbei gibt es gewisse Befreiungen für nahe Verwandte.<br/><br/>
+	Jeder Betreuer muss i.d.R. einmal im Jahr einen Bericht über die persönlichen Verhältnisse des Betreuten anfertigen.<br/><br/>
+	Der Betreuer darf keine Schenkungen aus dem Vermögen des Betreuten vornehmen mit Ausnahme sog. Anstands- oder Sittlichkeitsschenkungen. Im Zweifel ist vorher Rücksprache mit dem Betreuungsgericht zu nehmen.<br/><br/>
+	Praxistipp: Kontoauszüge und Belege immer aufbewahren, auch bei befreiter Betreuung, Auszahlungen an den Betreuten oder an Dritte immer quittieren lassen.</p>
+	<h3 id="Genehmigung">8. Genehmigungen des Betreuungsgerichts</h3>
+	<p>Der Betreuer muss vor einigen wichtigen Entscheidungen eine zusätzliche Genehmigung des Betreuungsgerichts einholen.<br/><br/>
+	Im Bereich der Personensorge sind dies: Einwilligung in eine gefährliche Operation oder Heilbehandlung (ggf. auch Nichteinwilligung in oder Widerruf von Behandlungsmaßnahmen), 
+	Einwilligung in eine Sterilisation, freiheitsentzeihende Unterbringung des Betreuten oder die Anwendung freiheitsentziehender Maßnahmen (z.B. Bettgitter), 
+	Kündigung oder Vermietung von Wohnraum des Betreuten, weitere Genehmigungen im Bereich des Familienrechts.<br/><br/>
+	Im Bereich Vermögenssorge: Verkauf oder Belastung von Grundbesitz des Betreuten, Aufnahme eines Darlehens, Wertpapiergeschäfte.<br/><br/>
+	Die Anlage von Betreutengeldern auf Sparbüchern, Festgeldern usw. und deren Abhebung ist grundsätzlich zu genehmigen außer bei befreiten Betreuern. Verfügungen über Girokonten sind genehmigungsfrei.<br/><br/>
+	Bei Bedarf können auch nicht befreite Betreuer eine allgemeine oder spezielle Ermächtigung zur Verfügung über größere Beträge beim Betreuungsgericht beantragen, z.B. zur monatlichen Überweisung von Heimkosten.</p>
+	<h3 id="Einwilligungsvorbehalt">9. Einwilligungsvorbehalt</h3>
+	<p>Da die betreute Person grundsätzlich geschäftsfähig bleibt, kann die weiterhin Geschäfte und Verträge abschließen und es kann zu konkurrierendem Handeln zwischen Betreuer und Betreutem kommen.<br/><br/>
+	Falls dadurch die Gefahr besteht, dass sich der Betreute erheblich schädigt, kann der Betreuer beim Betreuungsgericht einen sog. Einwilligungsvorbehalt beantragen. 
+	Dieser wird in einem gesonderten Verfahren geprüft und hat zur Folge, dass für bestimmte Willenserklärungen des Betreuten die Zustimmung des Betreuers erforderlich ist.<br/><br/>
+	In der Praxis wird ein Einwilligungsvorbehalt oft angeordnet, um psychisch kranke, geistig behinderte oder demenzkranke Menschen vor nachteiligen Verträgen oder Geldgeschäften zu schützen.</p>
+	<h3 id="Ansprüche">10. Ansprüche des Betreuers</h3>
+	<p>Ehrenamtliche Betreuer haben Anspruch auf eine Aufwandspauschale von 399,- € jährlich oder können sich ihre Aufwendungen auf konkreten Nachweis erstatten lassen. 
+	Nur bei erheblichem Vermögen und besonderem Umfang oder Schwierigkeit der Betreuung können Ehrenamtliche eine Ermessensvergütung erhalten, die das Betreuungsgericht nur auf Antrag gewährt.<br/><br/>
+	Ehrenamtliche Betreuer sind gesetzlich unfall- und haftpflichtversichert und haben einen Beratungsanspruch gegenüber Betreuungsbehörden, Betreuungsvereinen und dem Betreuungsgericht.<br/><br/>
+	Berufsbetreuer erhalten je nach Ausbildung eine Stundenvergütung von 27,- bis 44,- Euro. Die Anzahl der abrechenbaren Stunden ist gesetzlich festgeschrieben und bewegt sich zwischen 2 und 8,5 Stunden monatlich. 
+	Kriterien sind dabei Aufenthaltsort der betreuten Person (Heim oder Wohnung), Vermögenssituation sowie Dauer der Betreuungsmaßnahme.<br/><br/>
+	Bezahlen muss die Kosten der Betreute selbst, sofern sein Vermögen und Einkommen nicht unter den Sozialhilfeschongrenzen liegt. 
+	Aufwendungsersatz oder Betreuungsvergütung fallen bei einem Gesamtvermögen über 2.600,- € bzw. einem Einkommen von mtl. 808,- € (Stand 2016) zzgl. Kosten der Unterkunft an. 
+	Gerichtsgebühren (dazu gehören auch die Kosten des Betreuungsgutachtens) fallen erst ab 25.000,- € Gesamtvermögen an.</p>
+	<h3 id="Ende">11.  Ende der Betreuung</h3>
+	<p>Die Betreuung endet mit dem Tod der betreuten Person. Der Betreuer ist nicht verpflichtet oder berechtigt, die Bestattung des Betreuten zu regeln, es sei denn er ist Erbe oder wurde vom Betreuten hierzu ausdrücklich ermächtigt. 
+	Zuständig sind die Erben und die Angehörigen, notfalls auch das jeweilige Ordnungsamt. Nur bei Gefahr im Verzug sind Eilmaßnahmen zu treffen bzgl. der Wohnung, Versorgung von Haustieren o.ä.<br/><br/>
+	Der Betreuer muss einen Schlussabrechnung erstellen und seinen Betreuerausweis an das Betreuungsgericht zurückgeben.<br/><br/>
+	Die Betreuung ist auch schon vorher aufzuheben, wenn deren Voraussetzungen nicht mehr vorliegen (z.B. durch Besserung des Gesundheitszustands).</p>
+	<h3 id="Betreuungsgerichte">12. Betreuungsgerichte</h3>
+	<p><b>Zuständig für alle im Landkreis Heidenheim anhängigen Betreuungsverfahren ist das Amtsgericht Heidenheim, Olgastr, 22, 89518 Heidenheim.</b><br/><br/>
+	Sie erreichen die jeweiligen Stellen unter folgenden Durchwahlen:<br/><br/>
+	<u>Serviceeinheit in Betreuungssachen:</u><br/><br/>
+	Telefon: <a href="tel:07321/381720">07321 – 381720</a>  - zuständig für Buchstaben:<br/>
+	D, G, H, I, J, L, Q, R, S, T, W, Za – Zm (Name der betroffenen Person)<br/><br/>
+	Telefon: <a href="tel:07321/381721">07321 – 381721</a> -  zuständig für Buchstaben:<br/>
+	A, B, C, E, F, K, M, N, O, P, U, V, X, Y, Zn – Zz <br/><br/>
+	Fax: 07321 – 381700 (alle Buchstaben)<br/><br/>
+	<u>Serviceeinheit Unterbringungssachen und freiheitsentziehende Maßnahmen:</u><br/><br/>
+	Telefon: <a href="tel:07321/381214">07321 – 381214</a> (alle Buchstaben)<br/>
+	Fax allgemein: 07321 – 381234<br/><br/>
+	E-Mail allgemein: <a href="mailto:poststelle@AGHeidenheim.justiz.bwl.de">poststelle@AGHeidenheim.justiz.bwl.de</a><br/>
+	Webseite: <a href="http://www.amtsgericht-heidenheim.de/pb/,Lde/Startseite/Amtsgericht/Abteilungen+_+Serviceeinheiten">http://www.amtsgericht-heidenheim.de/pb/,Lde/Startseite/Amtsgericht/Abteilungen+_+Serviceeinheiten</a></p>
+	<h2>Formulare</h2>
+	<a href="localhost">Ich bin ein Beispieldownload-Link<br/></a>
+	<a href="localhost">Ich bin ein zweiter Beispieldownload-Link</a>
 </main>
 
 <?php
