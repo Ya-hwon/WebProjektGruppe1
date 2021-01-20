0fc7d8e8469673f42ef00f5b8e0bf55e53f7adca
Yahwon <>
2021-01-20 16:10:30 +0100

    added datenschutz raw

---
+++ datenschutz.php
@@ -0,0 +1,62 @@
+<?php
+include('./header.php');
+?>
+
+<link rel="stylesheet" href="/src/css/datenschutz.css">
+
+<main>
+  <h2>Datenschutzhinweise</h2><br/><br/>
+  <p>Den Schutz Ihrer Daten nehmen wir sehr ernst. Selbstverständlich beachten wir alle gesetzlichen Regelungen, insbesondere das Bundesdatenschutzgesetz (BDSG) und die Europäische Datenschutzgrundverordnung (DSGVO). Niemals werden wir Ihre Daten wirtschaftlich verwerten oder gar verkaufen. Im Folgenden wollen wir den Umgang mit Ihren Daten genauer darlegen:</p><br/>
+  <h3>1. Name und Kontaktdaten des für die Verarbeitung Verantwortlichen</h3>
+  <p>Verantwortlicher: Betreuungsverein Heidenheim e.V., <a href="https://maps.google.com/?q=Felsenstr. 36, 89518 Heidenheim" target="_blank">Felsenstr. 36, 89518 Heidenheim</a>
+  <br/>E-Mail: <a href="mailto:info@btv-hdh.de">info@btv-hdh.de</a></p>
+  <br/>
+  <h3>2. Erhebung und Speicherung personenbezogener Daten sowie Art und Zweck von deren Verwendung</h3>
+  <p>Beim Aufrufen unserer Website www.btv-hdh.de werden durch den auf Ihrem Endgerät zum Einsatz kommenden Browser automatisch Informationen an den Server unserer Website gesendet. Diese Informationen werden temporär in einem sog. Logfile gespeichert. Folgende Informationen werden dabei ohne Ihr Zutun erfasst und bis zur automatisierten Löschung gespeichert:</p>
+  <p>
+- IP-Adresse des anfragenden Rechners,<br/>
+- Datum und Uhrzeit des Zugriffs<br/>
+- Name und URL der abgerufenen Datei,<br/>
+- Website, von der aus der Zugriff erfolgt (Referrer-URL),<br/>
+- verwendeter Browser und ggf. das Betriebssystem Ihres Rechners sowie der Name Ihres Access-Providers.</p>
+<p>Die genannten Daten werden durch uns zu folgenden Zwecken verarbeitet:</p>
+<p>- Gewährleistung eines reibungslosen Verbindungsaufbaus der Website,<br/>
+- Gewährleistung einer komfortablen Nutzung unserer Website,<br/>
+- Auswertung der Systemsicherheit und -stabilität sowie zu weiteren administrativen Zwecken.</p>
+<p>
+Die Rechtsgrundlage für die Datenverarbeitung ist Art. 6 Abs. 1 S. 1 lit. f DSGVO. Unser berechtigtes Interesse folgt aus oben aufgelisteten Zwecken zur Datenerhebung. In keinem Fall verwenden wir die erhobenen Daten zu dem Zweck, Rückschlüsse auf Ihre Person zu ziehen.</p>
+<br/>
+<h3>3. Weitergabe von Daten</h3>
+<p>Eine Übermittlung Ihrer persönlichen Daten an Dritte zu anderen als den im Folgenden aufgeführten Zwecken findet nicht statt.
+Wir geben Ihre persönlichen Daten nur an Dritte weiter, wenn:</p>
+<p>- Sie Ihre nach Art. 6 Abs. 1 S. 1 lit. a DSGVO ausdrückliche Einwilligung dazu erteilt haben,<br/>
+- die Weitergabe nach Art. 6 Abs. 1 S. 1 lit. f DSGVO zur Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen erforderlich ist und kein Grund zur Annahme besteht, dass Sie ein überwiegendes schutzwürdiges Interesse an der Nichtweitergabe Ihrer Daten haben,<br/>
+- für den Fall, dass für die Weitergabe nach Art. 6 Abs. 1 S. 1 lit. c DSGVO eine gesetzliche Verpflichtung besteht, sowie<br/>
+- dies gesetzlich zulässig und nach Art. 6 Abs. 1 S. 1 lit. b DSGVO für die Abwicklung von Vertragsverhältnissen mit Ihnen erforderlich ist.<br/>
+- Bei der Verwendung des Spendenbuttons werden Sie auf die Website der Spendenplattform „Unsere Hilfe zählt“ der Heidenheimer Zeitung bzw. <a href="https://betterplace.org" target="_blank">betterplace.org</a> weitergeleitet. Diese Seite verwendet u.a. Cookies, Google Analytics, Facebook-Plugins usw. Wir verweisen hierzu ausdrücklich auf die Datenschutzerklärung von betterplace.org: <a href="https://www.betterplace.org/c/regeln/datenschutz" target="_blank">betterplace.org/c/regeln/datenschutz</a> </p>
+<br/>
+<h3>4. Betroffenenrechte</h3>
+<p>Sie haben das Recht:<br/><br/>
+- gemäß Art. 15 DSGVO Auskunft über Ihre von uns verarbeiteten personenbezogenen Daten zu verlangen. Insbesondere können Sie Auskunft über die Verarbeitungszwecke, die Kategorie der personenbezogenen Daten, die Kategorien von Empfängern, gegenüber denen Ihre Daten offengelegt wurden oder werden, die geplante Speicherdauer, das Bestehen eines Rechts auf Berichtigung, Löschung, Einschränkung der Verarbeitung oder Widerspruch, das Bestehen eines Beschwerderechts, die Herkunft ihrer Daten, sofern diese nicht bei uns erhoben wurden, sowie über das Bestehen einer automatisierten Entscheidungsfindung einschließlich Profiling und ggf. aussagekräftigen Informationen zu deren Einzelheiten verlangen<br/>
+- gemäß Art. 16 DSGVO unverzüglich die Berichtigung unrichtiger oder Vervollständigung Ihrer bei uns gespeicherten personenbezogenen Daten zu verlangen;<br/>
+- gemäß Art. 17 DSGVO die Löschung Ihrer bei uns gespeicherten personenbezogenen Daten zu verlangen, soweit nicht die Verarbeitung zur Ausübung des Rechts auf freie Meinungsäußerung und Information, zur Erfüllung einer rechtlichen Verpflichtung, aus Gründen des öffentlichen Interesses oder zur Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen erforderlich ist;<br/>
+- gemäß Art. 18 DSGVO die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen, soweit die Richtigkeit der Daten von Ihnen bestritten wird, die Verarbeitung unrechtmäßig ist, Sie aber deren Löschung ablehnen und wir die Daten nicht mehr benötigen, Sie jedoch diese zur Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen benötigen oder Sie gemäß Art. 21 DSGVO Widerspruch gegen die Verarbeitung eingelegt haben;<br/>
+- gemäß Art. 20 DSGVO Ihre personenbezogenen Daten, die Sie uns bereitgestellt haben, in einem strukturierten, gängigen und maschinenlesebaren Format zu erhalten oder die Übermittlung an einen anderen Verantwortlichen zu verlangen;<br/>
+- gemäß Art. 7 Abs. 3 DSGVO Ihre einmal erteilte Einwilligung jederzeit gegenüber uns zu widerrufen. Dies hat zur Folge, dass wir die Datenverarbeitung, die auf dieser Einwilligung beruhte, für die Zukunft nicht mehr fortführen dürfen und<br/>
+- gemäß Art. 77 DSGVO sich bei einer Aufsichtsbehörde zu beschweren. In der Regel können Sie sich hierfür an die Aufsichtsbehörde Ihres üblichen Aufenthaltsortes oder Arbeitsplatzes oder unseres Vereinssitzes wenden.</p>
+<br/>
+<h3>5. Wiederspruchsrecht</h3>
+<p>Sofern Ihre personenbezogenen Daten auf Grundlage von berechtigten Interessen gemäß Art. 6 Abs. 1 S. 1 lit. f DSGVO verarbeitet werden, haben Sie das Recht, gemäß Art. 21 DSGVO Widerspruch gegen die Verarbeitung Ihrer personenbezogenen Daten einzulegen, soweit dafür Gründe vorliegen, die sich aus Ihrer besonderen Situation ergeben oder sich der Widerspruch gegen Direktwerbung richtet. Im letzteren Fall haben Sie ein generelles Widerspruchsrecht, das ohne Angabe einer besonderen Situation von uns umgesetzt wird. Möchten Sie von Ihrem Widerrufs- oder Widerspruchsrecht Gebrauch machen, genügt eine E-Mail an: <a href="mailto:info@btv-hdh.de">info@btv-hdh.de</a></p>
+<br/>
+<h3>6. Datensicherheit</h3>
+<p>Wir bedienen uns geeigneter technischer und organisatorischer Sicherheitsmaßnahmen, um Ihre Daten gegen zufällige oder vorsätzliche Manipulationen, teilweisen oder vollständigen Verlust, Zerstörung oder gegen den unbefugten Zugriff Dritter zu schützen. Unsere Sicherheitsmaßnahmen werden entsprechend der technologischen Entwicklung fortlaufend verbessert.</p>
+<br/>
+<h3>7. Aktualität und Änderung dieser Datenschutzerklärung</h3>
+<p>Diese Datenschutzerklärung ist aktuell gültig und hat den Stand Juni 2018.<br/>
+Durch die Weiterentwicklung unserer Website und Angebote darüber oder aufgrund geänderter gesetzlicher beziehungsweise behördlicher Vorgaben kann es notwendig werden, diese Datenschutzerklärung zu ändern. Die jeweils aktuelle Datenschutzerklärung kann jederzeit auf der Website unter <a href="http://www.btv-hdh.de/datenschutz">http://www.btv-hdh.de/datenschutz</a> von Ihnen abgerufen und ausgedruckt werden.</p>
+</main>
+
+
+<?php
+include('./footer.php');
+?>
