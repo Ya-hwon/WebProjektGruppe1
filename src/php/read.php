38f47a85411520853e59b27434ef9c27bbb3a248
Yahwon <>
2021-01-22 10:29:59 +0100

    'unserverein' cards are now generated from csv

--- src/php/read.php
+++ src/php/read.php
@@ -1,16 +1,17 @@
 <?php
 
 function readFromCSV($fName, $dataHandler){
-  $row = 1;
-  if (($handle = fopen("/src/csv/"$fName, "r")) !== FALSE) {
+  $row = 0;
+  if (($handle = fopen("src/csv/".$fName, "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000)) !== FALSE) {// TODO: adjust len?
+        $row++;
+        if($row==1)continue;
         $num = count($data);
         $dataHandler($num, $data);
         /*echo "<p> $num fields in line $row: <br /></p>\n";
         for ($c=0; $c < $num; $c++) {
             echo $data[$c] . "<br />\n";
         }*/
-        $row++;
     }
     fclose($handle);
   }
--- unserverein.php
+++ unserverein.php
@@ -1,99 +1,43 @@
 <?php
+include('src/php/read.php');
 include('./header.php');
+function mitarbeiterHandler($num, $data){
+  if($num<>7)return;
+      echo '<div class="card mb-3" style="max-width: 540px">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img
+              src="src/img/'.$data[2].'"
+              alt="'.$data[3].'"
+              class="img-fluid"/>
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h4 class="card-title">'.$data[0].'</h4>
+              <p class="card-subtitle">'.$data[1].'</p>
+              <p class="card-text">
+                Tel.: '.$data[5].'<br/>
+                Fax: '.$data[6].'</p>
+                <button type="button" class="btn btn-primary">Email</button>
+            </div>
+          </div>
+        </div>
+      </div>';
+}
 ?>
 
 <link rel="stylesheet" href="src/css/unserverein.css">
 
 <main>
     <h2>Mitarbeiter</h2>
-    
-<div class="card mb-3" style="max-width: 540px">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img
-        src="src/img/placeholder-person.jpg"
-        alt="Foto von Stefan Kauffmann"
-        class="img-fluid"/>
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h4 class="card-title">Stefan Kauffmann</h4>
-        <p class="card-subtitle">Geschäftsführer</p>
-        <p class="card-text">
-          Tel.: 07321 943060<br/>
-          Fax: 07321 9469577</p>
-          <button type="button" class="btn btn-primary">Email</button>
-      </div>
-    </div>
-  </div>
-</div>
-    
-<div class="card mb-3" style="max-width: 540px">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img
-        src="src/img/placeholder-person.jpg"
-        alt="Foto von Heide Bolsinger"
-        class="img-fluid"/>
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h4 class="card-title">Heide Bolsinger</h4>
-        <p class="card-subtitle">Vereinsbetreuerin</p>
-        <p class="card-text">
-          Tel.: 07321 943062<br/>
-          Fax: 07321 9469577</p>
-          <button type="button" class="btn btn-primary">Email</button>
-      </div>
-    </div>
-  </div>
-</div>
-    
-<div class="card mb-3" style="max-width: 540px">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img
-        src="src/img/placeholder-person.jpg"
-        alt="Foto von Michaela Schmidt"
-        class="img-fluid"/>
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h4 class="card-title">Michaela Schmidt</h4>
-        <p class="card-subtitle">Vereinsbetreuerin</p>
-        <p class="card-text">
-          Tel.: 07321 9469578<br/>
-          Fax: 07321 9469577</p>
-          <button type="button" class="btn btn-primary">Email</button>
-      </div>
-    </div>
-  </div>
-</div>
-    
-<div class="card mb-3" style="max-width: 540px">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img
-        src="src/img/placeholder-person.jpg"
-        alt="Foto von Christine Bäuerle"
-        class="img-fluid"/>
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h4 class="card-title">Christine Bäuerle</h4>
-        <p class="card-subtitle">Verwaltung</p>
-        <p class="card-text">
-          Tel.: 07321 943061<br/>
-          Fax: 07321 9469577</p>
-          <button type="button" class="btn btn-primary">Email</button>
-      </div>
-    </div>
-  </div>
-</div>
-    
+
+<?php
+  readFromCSV("mitarbeiter.csv",'mitarbeiterHandler');
+?>
+
 <h2><br/>Vorstand</h2>
 <img id="photo-vorstand" src="src/img/vorstand.jpg" alt="Gruppenfoto des Vorstands">
-    
+
 </main>
 
 
