ea5016c10a04f55a3663dc85259785a34d55ad7a
Sarah Hägele <sarah.haegele01@gmail.com>
2021-02-09 11:06:05 +0100

    Fixed speeling error + name of file
    - Fixed a spelling error
    - Corrected file name: Satzung.pdf

--- unserVerein.php
+++ unserVerein.php
@@ -66,14 +66,14 @@ Auch ohne Übernahme einer ehrenamtlichen Betreuung können Sie unsere Arbeit du
 </div>
 
 <h2 id="Mitarbeit">Mitarbeit</h2>
-<p><b>Sie können es sich vostellen, ehrenamtlicher Betreuer zu werden?</b><br/>Mit einem Klick auf den folgenden Button können Sie sich weiter über die ehrenamtliche Betreuung informieren.</p>
+<p><b>Sie können es sich vorstellen, ehrenamtlicher Betreuer zu werden?</b><br/>Mit einem Klick auf den folgenden Button können Sie sich weiter über die ehrenamtliche Betreuung informieren.</p>
 <div class="button-mitglied-wrapper">
     <a href="https://www.btv-hdh.de/mitgliedschaft.html" target="_blank"><button type="button" class="btn btn-primary">Betreuer werden</button></a>
 </div>
 
 <h2>Satzung</h2>
 <div class="satzung-wrapper">
-    <a href="/src/doc/SATZ.pdf" download="vereinssatzung"><p><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+    <a href="/src/doc/Satzung.pdf" download="vereinssatzung"><p><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
         <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
         <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
     </svg> Vereinssatzung.pdf</p></a>
