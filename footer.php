461fb2a81452c42a336093622ede09bdb846b6ed
nicolasstyrnol <77490395+nicolasstyrnol@users.noreply.github.com>
2021-02-25 08:16:43 +0100

    bugfix

--- footer.php
+++ footer.php
@@ -1,25 +1,37 @@
-<footer>
-    
-    <div class="footer-content--wrapper">
-        <div class="footer-content--menu">
-            <h4>Menü</h4>
-            <a class="footer--links" href="./"><p>Home</p></a>
-            <a class="footer--links" href="./unserVerein#Mitarbeiter"><p>Unser Verein</p></a>
-            <a class="footer--links" href="./betreuung#Informationen"><p>Betreuung</p></a>
-            <a class="footer--links" href="./vorsorge#Informationen"><p>Vorsorge</p></a>
-        </div>
-        <div class="footer-content--legal">
-            <h4>Rechtliches</h4>
-            <a class="footer--links" href="./impressum"><p>Impressum</p></a>
-            <a class="footer--links" href="./datenschutz"><p>Datenschutz</p></a>
-            <a class="footer--links" href="./kontakt"><p>Kontakt</p></a>
-        </div>
-    </div>
-    <hr/>
-    <div class="footer-content--copyright">
-        <p>Copyright © 2021 All Rights Reserved</p>
-    </div>
-    
-    
-</footer>
+        <footer>
+           <div class="footer-content--wrapper">
+              <div class="footer-content--menu">
+                 <h4>Menü</h4>
+                 <a class="footer--links" href="./">
+                    <p>Home</p>
+                 </a>
+                 <a class="footer--links" href="./unserVerein#Mitarbeiter">
+                    <p>Unser Verein</p>
+                 </a>
+                 <a class="footer--links" href="./betreuung#Informationen">
+                    <p>Betreuung</p>
+                 </a>
+                 <a class="footer--links" href="./vorsorge#Informationen">
+                    <p>Vorsorge</p>
+                 </a>
+              </div>
+              <div class="footer-content--legal">
+                 <h4>Rechtliches</h4>
+                 <a class="footer--links" href="./impressum">
+                    <p>Impressum</p>
+                 </a>
+                 <a class="footer--links" href="./datenschutz">
+                    <p>Datenschutz</p>
+                 </a>
+                 <a class="footer--links" href="./kontakt">
+                    <p>Kontakt</p>
+                 </a>
+              </div>
+           </div>
+           <hr/>
+           <div class="footer-content--copyright">
+              <p>Copyright © 2021 All Rights Reserved</p>
+           </div>
+        </footer>
+    </body>
 </html>
--- header.php
+++ header.php
@@ -1,54 +1,55 @@
-    <header class="d-flex flex-row align-items-center">
-        <button class="nav-toggler" id="nav-toggler">
-            <span></span>
-            <span></span>
-            <span></span>
-        </button>
-        <div class="site-branding--logo">
-            <a href="./index"><img src="src/img/logo_white.png" alt="Logo des Betreuungsverein Heidenheim e.V." /></a>
-        </div>
-
-        <div class="site-branding--name">
-            <a href="./index"><h1>Betreuungsverein Heidenheim e.V.</h1></a>
-        </div>
-
-        <nav id="nav" class="d-flex align-items-center">
-            <ul class="d-flex">
-                <li>
-                    <a href="./">Home</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Unser Verein
-                    </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="./unserVerein#Mitarbeiter">Mitarbeiter</a>
-                        <a class="dropdown-item" href="./unserVerein#Vorstand">Vorstand</a>
-                        <a class="dropdown-item" href="./unserVerein#Mitglied">Mitglied werden</a>
-                        <a class="dropdown-item" href="./unserVerein#Mitarbeit">Mitarbeit</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Betreuung
-                    </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="./betreuung#Informationen">Informationen</a>
-                        <a class="dropdown-item" href="./betreuung#Formulare">Formulare</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Vorsorge
-                    </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="./vorsorge#Informationen">Informationen</a>
-                        <a class="dropdown-item" href="./vorsorge#Formulare">Formulare</a>
-                    </div>
-                </li>
-                <li>
-                    <a href="./kontakt">Kontakt</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
+<body>
+   <header class="d-flex flex-row align-items-center">
+      <button class="nav-toggler" id="nav-toggler">
+      <span></span>
+      <span></span>
+      <span></span>
+      </button>
+      <div class="site-branding--logo">
+         <a href="./index"><img src="src/img/logo_white.png" alt="Logo des Betreuungsverein Heidenheim e.V." /></a>
+      </div>
+      <div class="site-branding--name">
+         <a href="./index">
+            <h1>Betreuungsverein Heidenheim e.V.</h1>
+         </a>
+      </div>
+      <nav id="nav" class="d-flex align-items-center">
+         <ul class="d-flex">
+            <li>
+               <a href="./">Home</a>
+            </li>
+            <li class="nav-item dropdown">
+               <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               Unser Verein
+               </a>
+               <div class="dropdown-menu">
+                  <a class="dropdown-item" href="./unserVerein#Mitarbeiter">Mitarbeiter</a>
+                  <a class="dropdown-item" href="./unserVerein#Vorstand">Vorstand</a>
+                  <a class="dropdown-item" href="./unserVerein#Mitglied">Mitglied werden</a>
+                  <a class="dropdown-item" href="./unserVerein#Mitarbeit">Mitarbeit</a>
+               </div>
+            </li>
+            <li class="nav-item dropdown">
+               <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               Betreuung
+               </a>
+               <div class="dropdown-menu">
+                  <a class="dropdown-item" href="./betreuung#Informationen">Informationen</a>
+                  <a class="dropdown-item" href="./betreuung#Formulare">Formulare</a>
+               </div>
+            </li>
+            <li class="nav-item dropdown">
+               <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               Vorsorge
+               </a>
+               <div class="dropdown-menu">
+                  <a class="dropdown-item" href="./vorsorge#Informationen">Informationen</a>
+                  <a class="dropdown-item" href="./vorsorge#Formulare">Formulare</a>
+               </div>
+            </li>
+            <li>
+               <a href="./kontakt">Kontakt</a>
+            </li>
+         </ul>
+      </nav>
+   </header>
